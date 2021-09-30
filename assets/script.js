be6e7d39ca3caed2c4953afa70264fec09cf1592
zachary <zacharyberdell@gmail.com>
2021-09-30 16:57:29 -0600

    Creating the function to retrieve the weather data.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
- 
+ var searchButton = $("#search-button");
+
+
+ //Add function to get the neccessary information when the user searches the site.
+ citySearch.click(function(){
+    var cityName = $(this).siblings("#city-search").val();
+
+    $.ajax("api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=b088518c8339bc2fb89882ee7de2a221"){}
+ })
+
+ //outputs data on the dashboard
+
+ //create cards that display the weather
+
+ //Catch for if the input isn't a valid city
+
