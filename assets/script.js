a8e1135bdac406b9b84b1ed27e0ddc3db0157116
zachary <zacharyberdell@gmail.com>
2021-10-06 09:10:43 -0600

    Cards now output most of the required data.

--- assets/script.js
+++ assets/script.js
@@ -18,15 +18,16 @@ function apiCall(cityName){
       //Needed variable declaration
       var lat = result.data[0].latitude;
       var long = result.data[0].longitude;
+      var addressName = result.data[0].name
 
       //api call to get the weather data
       $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&lang=en&units=imperial&exclude=hourly,minutely&appid=b088518c8339bc2fb89882ee7de2a221",
+        url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&lang=en&units=imperial&exclude=hourly,minutely&appid=b088518c8339bc2fb89882ee7de2a221",
         dataType: "json",
     
         success: function(result){
            //Displays the current weather inforamation
-           $("city-name").text(moment().format("(M/DD/yyyy)") + " " + cityName);
+           $("#city-name").text(moment().format("(M/DD/yyyy)") + " " + addressName);
            $("#current-temp").text(result.current.temp);
            $("#current-wind").text(result.current.wind_speed);
            $("#current-humidity").text(result.current.humidity);
@@ -34,13 +35,14 @@ function apiCall(cityName){
 
            //Creates the weather cards
            for(let i = 0; i<5; i++){
+             var dayInfo = result.daily[i + 1];
               //Creates the neccessary elements
               var weatherCard = $("<div>").addClass("weather-card card").hide();
               var cardDate = $("<h3>").addClass("card-date");
-              var weatherIcon = $("<i>").addClass("weather-icon");
-              var tempP = $("<p>").addClass("temp-p");
-              var windP = $("<p>").addClass("wind-p");
-              var humidP = $("<p>").addClass("humid-p");
+              var weatherIcon = $("<i>").addClass("weather-icon").attr("src", "http://openweathermap.org/img/w/" + dayInfo.weather[0].icon + ".png");
+              var tempP = $("<p>").addClass("temp-p").text("temp: " + dayInfo.temp.day);
+              var windP = $("<p>").addClass("wind-p").text("wind: " + dayInfo.wind_speed);
+              var humidP = $("<p>").addClass("humid-p").text("humidity: " + dayInfo.humidity + "%");
               //Appends the neccessary elements
               weatherCard.append(cardDate, weatherIcon, tempP, windP, humidP);
               $("#block-holder").append(weatherCard);
